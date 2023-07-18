fcc4c0632c46433ca295aeb8dff620ece16b796d
JuanGM <119081982+JuanGMN@users.noreply.github.com>
2023-07-18 15:09:33 -0600

    Add files via upload

---
+++ 1/js/contact.js
@@ -0,0 +1,21 @@
+
+  const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_8fztehe';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
